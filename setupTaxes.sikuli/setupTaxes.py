da8843cb20b35e9c7bc4e0ed500c88a2ee707bfc
Tom Hinds <thinds66@yahoo.com>
2014-07-31 10:09:33 -0400

    set up taxes

--- setupTaxes.sikuli/setupTaxes.py
+++ setupTaxes.sikuli/setupTaxes.py
@@ -2,6 +2,275 @@ from sikuli import *
 import logging
 import myTools
 
+
+#---------------------------------------------------#
+def Create_Jurisdictions():
+#---------------------------------------------------#
+
+    logging.debug('- Create_Jurisdiction')
+
+    # move to jurisdictions
+    myTools.pressF6(2)
+    time.sleep(1)
+
+    # new jurisdication for Zone 1
+    logging.debug('-- new jurisdictions')  
+    type("n",KeyModifier.ALT)
+    time.sleep(1)
+    type("o")
+    myTools.pressTAB(1)
+    type("Zone1")
+    myTools.pressTAB(1)
+    type("One")
+    time.sleep(1)
+    
+    # OK
+    type(Key.ENTER)
+
+#---------------------------------------------------#
+def Create_RateRules():
+#---------------------------------------------------#
+    logging.debug('- Create_RateRules')
+
+    # switch to rate rules page
+    myTools.pressSHIFTF6(1)
+    time.sleep(1)
+
+    Create_TimeOnly_RateRule()
+    Create_ExpenseOnly_RateRule()
+    Create_Compound_RateRules()
+    Create_Minimum_RateRule()
+    Create_Maximum_RateRule()
+
+
+
+#---------------------------------------------------#
+def Create_TimeOnly_RateRule():
+#---------------------------------------------------#
+    logging.debug('- Create_TimeOnly_RateRule')
+
+    # new rule
+    type("n",KeyModifier.ALT)
+    type("Time Only")
+    myTools.pressTAB(1)
+    type("z")
+    time.sleep(1)
+    # rate
+    myTools.pressTAB(2)
+    type("4")
+    # options   
+    myTools.pressTAB(5)
+    type(Key.SPACE)
+    myTools.pressTAB(2)
+    type(Key.SPACE)
+    # save
+    myTools.pressTAB(1)
+    type(Key.SPACE)
+    time.sleep(1)
+    # export to all
+    type(Key.INSERT)
+    myTools.pressTAB(1)
+    type(Key.SPACE)    
+    time.sleep(1)   
+
+#---------------------------------------------------#
+def Create_ExpenseOnly_RateRule():
+#---------------------------------------------------#
+    logging.debug('- Create_ExpenseOnly_RateRule')
+
+    # new rule
+    type("n",KeyModifier.ALT)
+    type("Expense Only")
+    myTools.pressTAB(1)
+    type("z")
+    time.sleep(1)
+    # rate
+    myTools.pressTAB(2)
+    type("4.25")
+    # type
+    myTools.pressTAB(1)
+    type("e")
+    # options   
+    myTools.pressTAB(5)
+    type(Key.SPACE)
+    myTools.pressTAB(2)
+    type(Key.SPACE)
+    myTools.pressTAB(1)
+    # save
+    type(Key.SPACE)
+    time.sleep(1)
+    # export to all
+    type(Key.INSERT)
+    myTools.pressTAB(1)
+    type(Key.SPACE)    
+    time.sleep(1)
+
+#---------------------------------------------------#
+def Create_Compound_RateRules():
+#---------------------------------------------------#
+    logging.debug('- Create_Compound_RateRule')
+
+    # new rule
+    type("n",KeyModifier.ALT)
+    type("Compound2")
+    myTools.pressTAB(1)
+    type("z")
+    time.sleep(1)
+    # rate
+    myTools.pressTAB(2)
+    type("3")
+    myTools.pressTAB(5)
+    type(Key.SPACE)
+    myTools.pressTAB(2)
+    type(Key.SPACE)
+    # save
+    myTools.pressTAB(1)
+    type(Key.SPACE)
+    time.sleep(1)
+    # export to all
+    type(Key.INSERT)
+    myTools.pressTAB(1)
+    type(Key.SPACE)    
+    time.sleep(1)
+
+    # new rule
+    type("n",KeyModifier.ALT)
+    type("Compound1")
+    myTools.pressTAB(1)
+    type("z")
+    time.sleep(1)
+    # rate
+    myTools.pressTAB(2)
+    type("3.5")
+    # Type
+    myTools.pressTAB(1)
+    myTools.pressDOWN(2)  
+    # compound
+    myTools.pressTAB(2)    
+    type("com")
+    # options
+    myTools.pressTAB(1)
+    type(Key.SPACE)
+    myTools.pressTAB(2)
+    type(Key.SPACE)
+    # save
+    myTools.pressTAB(1)
+    type(Key.SPACE)
+    time.sleep(1)
+    # export to all
+    type(Key.INSERT)
+    myTools.pressTAB(1)
+    type(Key.SPACE)    
+    time.sleep(1)
+
+#---------------------------------------------------#
+def Create_Minimum_RateRule():
+#---------------------------------------------------#
+    logging.debug('- Create_Minimum_RateRule')
+
+    # new rule
+    type("n",KeyModifier.ALT)
+    type("Minimum")
+    myTools.pressTAB(1)
+    type("z")
+    time.sleep(1)
+    # rate
+    myTools.pressTAB(2)
+    type("4.5")
+    # minimum
+    myTools.pressTAB(4)
+    myTools.pressDOWN(1)
+    time.sleep(1)
+    # threshold
+    myTools.pressTAB(1)
+    type("1000")
+    # options
+    myTools.pressTAB(2)
+    type(Key.SPACE)
+    myTools.pressTAB(2)
+    type(Key.SPACE)
+    # save
+    myTools.pressTAB(1)
+    type(Key.SPACE)
+    time.sleep(1)
+    # export to all
+    type(Key.INSERT)
+    myTools.pressTAB(1)
+    type(Key.SPACE)    
+    time.sleep(1)
+
+#---------------------------------------------------#
+def Create_Maximum_RateRule():
+#---------------------------------------------------#
+    logging.debug('- Create_Maximum_RateRule')
+
+    # new rule
+    type("n",KeyModifier.ALT)
+    type("Maximum")
+    myTools.pressTAB(1)
+    type("z")
+    time.sleep(1)
+    # rate
+    myTools.pressTAB(2)
+    type("4.5")
+    # maximum
+    myTools.pressTAB(4)
+    myTools.pressDOWN(2)
+    time.sleep(1)
+    # threshold
+    myTools.pressTAB(1)
+    type("1000")   
+    # options
+    myTools.pressTAB(2)
+    type(Key.SPACE)
+    myTools.pressTAB(2)
+    type(Key.SPACE)
+    # save
+    myTools.pressTAB(1)
+    type(Key.SPACE)
+    time.sleep(1)
+    # export to all
+    type(Key.INSERT)
+    myTools.pressTAB(1)
+    type(Key.SPACE)    
+    time.sleep(1)
+
+#---------------------------------------------------#
+def Create_TaxProfile():
+#---------------------------------------------------#
+    logging.debug('- Create_TaxProfile')
+
+    type("1",KeyModifier.CTRL)
+    
+    # new profile    
+    type("n",KeyModifier.ALT)   
+    type("MyTaxes")
+    time.sleep(1)
+
+    # Time rule    
+    myTools.pressTAB(1)
+    type("t")
+    # Expense rule
+    myTools.pressTAB(1)
+    type("e")
+    # Compound rule
+    myTools.pressTAB(1)
+    type("com")
+    # Minimum rule
+    myTools.pressTAB(1)
+    type("min")
+    # Maximum rule
+    myTools.pressTAB(1)
+    type("max")
+
+    # OK
+    time.sleep(1)
+    type(Key.ENTER)    
+
+    # DONE
+    myTools.pressTAB(6)
+    type(Key.ENTER)
+
 #---------------------------------------------------#
 def Change_TaxRule():
 #---------------------------------------------------#
@@ -50,7 +319,7 @@ def Change_ClientSettings():
     
     myTools.pressTAB(3)   
     time.sleep(1)
-    type("g")
+    type("my")
 
     logging.debug('-- save client')
     type("s",KeyModifier.CTRL)
@@ -169,11 +438,21 @@ def Setup_Taxes():
     
     # make sure timeslips has focus
     myTools.getFocus()
-
     time.sleep(1)
-    Change_TaxRule()
+
+    # open taxes UI
+    logging.debug('-- open Taxes')
+    type("p",KeyModifier.ALT)   
+    type("t")
+    time.sleep(2)
+
+    Create_Jurisdictions()
+    Create_RateRules()
+    Create_TaxProfile()
+
+#    Change_TaxRule()
     Change_ClientSettings()
-    Change_TaskSettings()
-    Change_ExpenseSettings()
+#    Change_TaskSettings()
+#    Change_ExpenseSettings()
 
     myTools.sectionEndTimeStamp()
