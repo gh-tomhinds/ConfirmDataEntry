7f07c412cd6267924db5a6db46b4ec391e23f991
Tom Hinds <thinds66@yahoo.com>
2014-09-12 11:57:05 -0400

    Updated baseline reports

--- !reports/Baseline/renamefiles.bat
+++ !reports/Baseline/renamefiles.bat
@@ -1,6 +1,6 @@
 for %%i in ("*.csv") do (set fname=%%i) & call :rename
 goto :eof
 :rename
-::Cuts off 1st five chars
+::Cuts off 1st four chars, then appends prefix
 ren "%fname%" "%fname:~5%"
 goto :eof
