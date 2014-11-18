538fafeff94898bd7dcc2092fc461dd55ffcf19b
Tom Hinds <thinds66@yahoo.com>
2014-11-18 11:31:50 -0500

    renamed more billing arrangement files

---
+++ ba_InterimActivity.sikuli/ba_InterimActivity.py
@@ -0,0 +1,171 @@
+from sikuli import *
+import logging
+import myTools
+import createClient
+import ba__Common
+import ba__ReviewBills
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - #
+def BA_InterimActivity_Arrangement1():
+# - - - - - - - - - - - - - - - - - - - - - - - - - #
+
+    myTools.sectionStartTimeStamp("ba InterimAct1")
+    logging.debug("ba InterimAct1")
+
+# open client    
+    type("i",KeyModifier.CTRL)
+    time.sleep(1)
+    type("BA-InterimAct")
+    type(Key.ENTER)
+    time.sleep(1)
+
+# get to arrangement field for time
+    ba__Common.moveto_BAPage()
+    myTools.pressTAB(4)
+    
+# switch to Interim
+    type(Key.END)
+    
+# enter details    
+    type(Key.TAB)
+    type(Key.ENTER)
+    time.sleep(1) 
+    type(Key.DOWN)
+    time.sleep(1) 
+    type("o",KeyModifier.ALT)    
+    type("500")
+    time.sleep(1)    
+    type(Key.ENTER)
+    time.sleep(1)    
+    type(Key.ENTER)
+# save and close    
+    type("s",KeyModifier.CTRL)
+
+    type(Key.F4,KeyModifier.CTRL)
+    time.sleep(1)
+    type(Key.F4,KeyModifier.CTRL)
+
+    myTools.sectionEndTimeStamp()
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - #
+def BA_InterimActivity_Arrangement2():
+# - - - - - - - - - - - - - - - - - - - - - - - - - #
+
+    myTools.sectionStartTimeStamp("ba InterimAct2")
+    logging.debug("ba InterimAct2")
+
+# open client    
+    type("i",KeyModifier.CTRL)
+    time.sleep(1)
+    type("BA-InterimAct")
+    type(Key.ENTER)
+    time.sleep(1)
+
+# get to arrangement field for time
+    ba__Common.moveto_BAPage()
+    myTools.pressTAB(5)
+    
+# enter details    
+    type(Key.ENTER)
+    time.sleep(1)
+    type(Key.DOWN)
+    time.sleep(1)
+    type("o",KeyModifier.ALT)        
+    type("600")
+    time.sleep(1)    
+    type(Key.ENTER)
+    time.sleep(1)    
+    type(Key.ENTER)
+    
+# save and close    
+    type("s",KeyModifier.CTRL)
+    time.sleep(1)
+
+    if int(Settings.tsVersion) > 2014:
+        if exists("are_you_sure.png"):
+            type("y",KeyModifier.ALT)
+
+    type(Key.F4,KeyModifier.CTRL)
+    time.sleep(1)
+    type(Key.F4,KeyModifier.CTRL)    
+
+    myTools.sectionEndTimeStamp()
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - #
+def BA_InterimActivity_Arrangement3():
+# - - - - - - - - - - - - - - - - - - - - - - - - - #
+
+    myTools.sectionStartTimeStamp("ba InterimAct3")
+    logging.debug("ba InterimAct3")
+
+# open client    
+    type("i",KeyModifier.CTRL)
+    time.sleep(1)
+    type("BA-InterimAct")
+    type(Key.ENTER)
+    time.sleep(1)
+
+# get to arrangement field for time
+    ba__Common.moveto_BAPage()
+    myTools.pressTAB(5)
+    
+# enter details    
+    type(Key.ENTER)
+    time.sleep(1)
+    type(Key.DOWN)
+    time.sleep(1)
+    type("o",KeyModifier.ALT)        
+    type("700")
+    time.sleep(1)    
+    type(Key.ENTER)
+    time.sleep(1)    
+    type("u",KeyModifier.ALT)
+    type(Key.DOWN)
+    type(Key.ENTER)
+    
+# save and close    
+    type("s",KeyModifier.CTRL)
+    time.sleep(1)
+
+    if int(Settings.tsVersion) > 2014:
+        if exists("are_you_sure.png"):
+            type("y",KeyModifier.ALT)
+
+    type(Key.F4,KeyModifier.CTRL)
+    time.sleep(1)
+    type(Key.F4,KeyModifier.CTRL)    
+
+    myTools.sectionEndTimeStamp()
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - #
+def BA_InterimActivity():
+# - - - - - - - - - - - - - - - - - - - - - - - - - #
+
+    # create a new client    
+    createClient.Create_Client("BA-InterimAct","BA-InterimAct","Interim Activity FF","Interim Activity FF","Interim Activity FF")
+    # create some slips
+    ba__Common.BA_Create_Slips("BA-InterimAct")
+    # set up billing arrangement
+    BA_InterimActivity_Arrangement1() 
+    # print a bill to text
+    ba__Common.BA_Bill("BA-InterimAct",1)
+    # compare at bill values
+    ba__ReviewBills.Review_Bill("BA-InterimAct1")
+
+    # create some slips
+    ba__Common.BA_Create_Slips("BA-InterimAct")
+    # set up billing arrangement
+    BA_InterimActivity_Arrangement2() 
+    # print a bill to text
+    ba__Common.BA_Bill("BA-InterimAct",2)
+    # compare at bill values
+    ba__ReviewBills.Review_Bill("BA-InterimAct2")
+
+    # create some slips
+    ba__Common.BA_Create_Slips("BA-InterimAct")
+    # set up billing arrangement
+    BA_InterimActivity_Arrangement3() 
+    # print a bill to text
+    ba__Common.BA_Bill("BA-InterimAct",3)
+    # compare at bill values
+    ba__ReviewBills.Review_Bill("BA-InterimAct3")
---
+++ ba_SlipsRoundDol.sikuli/ba_SlipsRoundDol.py
@@ -0,0 +1,59 @@
+from sikuli import *
+import logging
+import myTools
+import createClient
+import ba__Common
+import ba__ReviewBills
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - #
+def BA_Round_Dollars():
+# - - - - - - - - - - - - - - - - - - - - - - - - - #
+
+    myTools.sectionStartTimeStamp("ba RoundDollars")
+    logging.debug("ba RoundDollars")
+
+# open client    
+    type("i",KeyModifier.CTRL)
+    time.sleep(1)
+    type("BA-RoundDol")
+    type(Key.ENTER)
+    time.sleep(1)
+
+# get to Rounding field
+    if int(Settings.tsVersion) < 2015:
+        myTools.pressF6(4)
+        time.sleep(1)
+        myTools.pressTAB(6)
+    else:
+        myTools.pressF6(7)
+        time.sleep(1)
+        myTools.pressTAB(5)               
+
+    time.sleep(1)
+    type("5") 
+    type(Key.TAB)
+    type("u")
+    
+# save and close    
+    type("s",KeyModifier.CTRL)
+
+    type(Key.F4,KeyModifier.CTRL)
+    time.sleep(1)
+    type(Key.F4,KeyModifier.CTRL)    
+
+    myTools.sectionEndTimeStamp()
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - #
+def BA_SlipsRoundDol():
+# - - - - - - - - - - - - - - - - - - - - - - - - - #
+
+    # create a new client    
+    createClient.Create_Client("BA-RoundDol","BA-RoundDol","Rounding Dollars","Rounding Dollars","Rounding Dollars")
+    # create some slips
+    ba__Common.BA_Create_Slips("BA-RoundDol")
+    # set up billing arrangement
+    BA_Round_Dollars() 
+    # print a bill to text
+    ba__Common.BA_Bill("BA-RoundDol",1)
+    # compare at bill values
+    ba__ReviewBills.Review_Bill("BA-RoundDol1")
