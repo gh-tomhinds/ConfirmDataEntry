dfc39067066391f601ebacfe10fac0444261ab42
Tom Hinds <thinds66@yahoo.com>
2015-03-02 16:56:13 -0500

    new report: statements

--- reports_CreateReports.sikuli/reports_CreateReports.py
+++ reports_CreateReports.sikuli/reports_CreateReports.py
@@ -5,7 +5,9 @@ import report_UDSlip
 import report_UDClient
 import report_UDInvoice
 import report_UDFunds
+import report_Statement
 import backup_Data
+
 
 #---------------------------------------------------#
 def create_UDReports():
@@ -21,6 +23,7 @@ def create_UDReports():
     report_UDInvoice.fCreate_InvoiceListFields()
     report_UDInvoice.fSort_InvoiceListFields()    
     report_UDFunds.fCreate_FundsListFields()
+    report_Statement.fImport_Statement()
 
     myTools.sectionEndTimeStamp()
     backup_Data.fBackup_Checkpoint("udReports")
--- reports_PostBill.sikuli/reports_PostBill.py
+++ reports_PostBill.sikuli/reports_PostBill.py
@@ -12,6 +12,7 @@ import report_ProdPeriod
 import report_ProfPeriod
 import report_Markup
 import report_SlipSummary
+import report_Statement
 import report_Taxes
 import report_TkCC
 import report_TkHistory
@@ -28,28 +29,30 @@ def fPrint_PostbillReports(pMonth,pAorB):
 
     # print various reports to check db values and calculations
 
-    repExt = pAorB + ".csv"
+    csvExt = pAorB + ".csv"
+    textExt = pAorB + ".txt"
 
-    report_AgedARBal.Print_ARAgedBal(pMonth,repExt)
-    report_UDSlip.fPrint_SlipListDetailed(pMonth,repExt)    
-    report_UDSlip.fPrint_SlipFields(pMonth,repExt)
-    report_UDClient.fPrint_ClientListHistory(pMonth,repExt)
-    report_UDFunds.fPrint_FundsListFields(pMonth,repExt)
-    report_UDInvoice.fPrint_InvoiceListFields(pMonth,repExt)
-    report_TkHistory.Print_TkHistory(pMonth,repExt)
-    report_TkCC.Print_TkCC(pMonth,repExt)
-    report_FundsBal.fPrint_FundsBal(pMonth,repExt)
-    report_Hold.Print_Hold(pMonth,repExt)
-    report_Taxes.Print_Taxes(pMonth,repExt)
-    report_TOWorksheet.Print_Worksheet(pMonth,repExt)    
-    report_Budgets.printCliBudget(pMonth,repExt)
-    report_Budgets.printTkBudget(pMonth,repExt)
-    report_Budgets.printFirmBudget(pMonth,repExt)
-    report_ProdPeriod.print_ProdPeriod(pMonth,repExt)
-    report_ProfPeriod.print_ProfPeriod(pMonth,repExt)
-    report_Markup.print_Markup(pMonth,repExt)
-    report_SlipSummary.print_SlipSummary(pMonth,repExt)
-    report_GLXfer.Print_GLXfer(pMonth,repExt)
+    report_AgedARBal.Print_ARAgedBal(pMonth,csvExt)
+    report_UDSlip.fPrint_SlipListDetailed(pMonth,csvExt)
+    report_UDSlip.fPrint_SlipFields(pMonth,csvExt)
+    report_UDClient.fPrint_ClientListHistory(pMonth,csvExt)
+    report_UDFunds.fPrint_FundsListFields(pMonth,csvExt)
+    report_UDInvoice.fPrint_InvoiceListFields(pMonth,csvExt)
+    report_TkHistory.Print_TkHistory(pMonth,csvExt)
+    report_TkCC.Print_TkCC(pMonth,csvExt)
+    report_FundsBal.fPrint_FundsBal(pMonth,csvExt)
+    report_Hold.Print_Hold(pMonth,csvExt)
+    report_Taxes.Print_Taxes(pMonth,csvExt)
+    report_TOWorksheet.Print_Worksheet(pMonth,csvExt)
+    report_Budgets.printCliBudget(pMonth,csvExt)
+    report_Budgets.printTkBudget(pMonth,csvExt)
+    report_Budgets.printFirmBudget(pMonth,csvExt)
+    report_ProdPeriod.print_ProdPeriod(pMonth,csvExt)
+    report_ProfPeriod.print_ProfPeriod(pMonth,csvExt)
+    report_Markup.print_Markup(pMonth,csvExt)
+    report_SlipSummary.print_SlipSummary(pMonth,csvExt)
+    report_Statement.fPrint_Statement(pMonth,txtExt)
+    report_GLXfer.Print_GLXfer(pMonth,csvExt)
 
 #    report_UDClient.fPrint_ClientListValues(pMonth,repExt)
 #    report_DaysToPay.print_DaysToPay(pMonth,repExt)
@@ -59,5 +62,5 @@ def fPrint_PostbillReports(pMonth,pAorB):
         logging.debug('SKIP FEE ALLOCATION REPORT')
         logging.debug('SKIP PAYMENT DISTR REPORT')        
     else:
-        report_FeeAlloc.Print_FeeAlloc(pMonth,repExt)
-        report_PayDistr.Print_PayDistr(pMonth,repExt)
+        report_FeeAlloc.Print_FeeAlloc(pMonth,csvExt)
+        report_PayDistr.Print_PayDistr(pMonth,csvExt)
