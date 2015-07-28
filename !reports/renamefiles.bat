cae789b1c5233790810acdb28c54b8087ad313ec
Tom Hinds <thinds66@yahoo.com>
2015-07-28 16:15:06 -0400

    renaming reports

--- !reports/renamefiles.bat
+++ !reports/renamefiles.bat
@@ -1,6 +1,6 @@
 for %%i in ("*.csv") do (set fname=%%i) & call :rename
-goto :eof
+exit /b
+
 :rename
-::Cuts off 1st four chars, then appends prefix
-ren "%fname%" "%fname:~5%"
-goto :eof
+ren "%fname%" "%fname:-2014.=.%"
+exit /b
